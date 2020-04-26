5725265b6eb0cb71a824f1a151acb78fd72ebf68
Patrícia Vieira <54964992+PatriciaV24@users.noreply.github.com>
2020-04-26 23:43:26 +0100

    Update game.h

--- game.h
+++ game.h
@@ -1,7 +1,7 @@
-void OpMenu();
-void OpRegras();
-void Regras();
-void Jogo();
+void OpMainMenu();
+void OpRules();
+void Rules();
+void Game();
 void AutoCoord(int npTipos[],int size);
 void InsertBoatA(int np,int npTipos[],BOARD *player,int size,SHIP *ships);
 void ManualCoord(int npTipos[],int size);
@@ -12,6 +12,7 @@ char ValModo();
 int ValTab();
 void NPieces(int *nptpieces,int tabu);
 void WaitForPlayer();
-void Menu();
+void MainMenu();
 void Pieces();
-void Title();
+void Title(); 
+ 
