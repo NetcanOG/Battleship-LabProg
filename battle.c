0d029a0f3d9a8b2f7fd8fff69d4a09366eff0c59
Patrícia Vieira <54964992+PatriciaV24@users.noreply.github.com>
2020-05-03 18:32:05 +0100

    Update battle.c

--- battle.c
+++ battle.c
@@ -4,118 +4,135 @@
 #include <ctype.h>
 #include <time.h>
 #include "structs.h"
-#include "game.h"
+#include "game.h" 
 
 //função que recebe a board de um jogador e retorna um int com o valor de peças de
 //barcos não destruídas (HP, Health Points)
 int getHP(BOARD* player){
-  int hp = 0, size = player->size;
+	int hp = 0, size = player->size;
 	size *= size;
 
 	for(int i = 0; i < size; i++){
-		if(player->map[i].posinfo == '1') hp++;
+		if(player->map[i].posinfo == '1') 
+			hp++;
 	}
 	return hp;
 }
 
 //Se na posição estiver um barco ou água, imprime 0 em ambos os casos. 2 se algum tiro tiver acertado, 3 se foi um tiro falhado
 void printEnemyBoard(BOARD* player){
-  int size = player->size;
+	int size = player->size;
 	printf("    ");
 	for(int i = 0; i < size; i++){
 		if(i < 10)
-		printf(" %d ", i);
+			printf(" %d ", i);
 		else
-		printf("%d ", i); 
-  }
-  printf("\n\n");
-  for(int y = 0; y < size; y++){
-    if(y < 10)
-    printf("  %d  ", y);
-    else
-    printf(" %d  ", y);  
-    for(int x = 0; x < size; x++){
-      switch (player->map[y*size+x].posinfo){
-        case '0': //Blue
-        printf("\033[0;34m");
-        printf("0  ");
-        printf("\033[0m");
-        break;
-        case '1': //Blue
-        printf("\033[0;34m");
-        printf("0  ");
-        printf("\033[0m");
-        break;
-        case '2': //Bold Red
-        printf("\033[1;31m");
-        printf("2  ");
-        printf("\033[0m");
-        break;
-        case '3': //Green
-        printf("\033[0;32m");
-        printf("3  ");
-        printf("\033[0m");
-        break;
-        default: //Branco (sem definições)
-        printf("%c  ", player->map[y*size + x].posinfo);
-        break;
-      }
-    }
-    printf("\n");
-  }
-  printf("\n");
+			printf("%d ", i); 
+	}
+	printf("\n\n");
+
+	for(int y = 0; y < size; y++){
+		if(y < 10)
+			printf("  %d  ", y);
+		else
+			printf(" %d  ", y);  
+
+		for(int x = 0; x < size; x++){
+		  switch (player->map[y*size+x].posinfo){
+		    case '0': //Blue
+			        printf("\033[0;34m");
+			        printf("0  ");
+			        printf("\033[0m");
+		    		break;
+		    case '1': //Blue
+			        printf("\033[0;34m");
+			        printf("0  ");
+			        printf("\033[0m");
+			        break;
+		    case '2': //Bold Red
+			        printf("\033[1;31m");
+			        printf("2  ");
+			        printf("\033[0m");
+		    		break;
+		    case '3': //Green
+			        printf("\033[0;32m");
+			        printf("3  ");
+			        printf("\033[0m");
+			        break;
+		    default: //Branco (sem definições)
+			        printf("%c  ", player->map[y*size + x].posinfo);
+			        break;
+		  }
+		}
+		printf("\n");
+	}
+	printf("\n");
 }
 
 int checkValidPosition(int size, int x, int y){ 
-  if(x < size && x >= 0 && y < size && y >= 0){
-    return 0;
+  	if(x < size && x >= 0 && y < size && y >= 0){
+    	return 0;
 	}
 	return 1;
 }
 
 //0 = water, 1 = piece without being hit, 2 = piece hit, 3 = missed shot
 void shootEnemy(BOARD* enemy){
-  int x, y;
-  printf("\nInput x and y for coordinates you want to attack!");
-	printf("\nx: ");
-	scanf("%d", &x);
-  printf("\ny: ");
-	scanf("%d", &y);
-	printf("\n");
+	int x, y;
+  	printf("\nColoque em  x e y as coordenadas que quer atacar!\n");
+	printf("X: ");	scanf("%d", &x); for(int c=0; (c=getchar()) != '\n' && c!= EOF;);
+  	printf("Y: ");	scanf("%d", &y); for(int c=0; (c=getchar()) != '\n' && c!= EOF;);
+	printf("\n\n\n");
+
 
 	if(checkValidPosition(enemy->size, x, y) == 0){
 		switch (enemy->map[y*enemy->size + x].posinfo){
 		case '0':
-			printf("Water hit!\n");
+			printf("\033[1;34m");
+			printf("Água atingida!\n\n");
 			enemy->map[y*enemy->size + x].posinfo = '3';
+			printf("\033[0m");
 			break;
 
 		case '1':
-			printf("Piece hit!\n");
+			printf("\033[1;31m");
+			printf("Peça atingida!\n\n");
 			enemy->map[y*enemy->size + x].posinfo = '2';
+			printf("\033[0m");
 			break;
 
 		case '2':
-			printf("You already hit a piece there! Try again.\n");
+			printf("\033[1;33m");
+			printf("Já atingiu está peça! Tente novamente.\n\n");
+			printf("\033[0m");
 			shootEnemy(enemy);
 			break;
 			
 		case '3':
-			printf("You already shot there and missed... Try again.\n");
+			printf("\033[1;33m");
+			printf("Você já atirou lá e errou ... Tente novamente.\n\n");
+			printf("\033[0m");
 			shootEnemy(enemy);
 			break;		
 		}
+		
 	}
 	else{
-		printf("\nInvalid input! Try again!\n");
+		printf("\033[1;33m");
+		printf("\nEntrada inválida! Tente novamente!\n");
+		printf("\033[0m");
 		shootEnemy(enemy);
 	}
+	
 }
 
 void playerTurn(BOARD* player, BOARD* enemy, int playernumber){
-  Title();
 	//mostrar qual o jogador
-	printf("\nJOGADOR %d\n", playernumber);
+
+  	printf("\033[1;32m");
+	printf("\t\t\t  JOGADOR %d: \n\n", playernumber);
+	printf("\033[0m");
+
 	//----------------------------
 	PrintBoard(player, player->size);
 	printEnemyBoard(enemy);
