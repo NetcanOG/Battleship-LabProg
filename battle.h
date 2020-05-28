296b51560a7e3531665825ca03b4db07c7da124a
Patrícia Vieira <54964992+PatriciaV24@users.noreply.github.com>
2020-05-28 09:52:21 +0100

    Update battle.h

--- battle.h
+++ battle.h
@@ -2,4 +2,4 @@ int getHP(BOARD* player);
 void printEnemyBoard(BOARD* player);
 int checkValidPosition(int size, int x, int y);
 void shootEnemy(BOARD* enemy);
-void playerTurn(BOARD* player, BOARD* enemy, int playernumber);
+void playerTurn(BOARD* player, BOARD* enemy, int playernumber, int HP);
