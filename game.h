3550d8cb081b605c9131f6039420e6a15e7456ff
Patrícia Vieira <54964992+PatriciaV24@users.noreply.github.com>
2020-05-28 12:28:51 +0100

    Update game.h

--- game.h
+++ game.h
@@ -1,14 +1,14 @@
 BOARD** OpMainMenu();
 void OpRules();
-void Rules();
+void Rules(); 
 BOARD** Game();
 BOARD** AutoCoord(int npTipos[],int size);
 void InsertBoatA(int np,int npTipos[],BOARD *player,int size,SHIP *ships);
 BOARD** ManualCoord(int npTipos[],int size);
-int InsertPiece(BOARD *player,SHIP piece,int size,int mx,int my,int orientation);
+int InsertPiece(BOARD *player,BOARD *apresent,SHIP piece,int size,int mx,int my,int orientation, char tipo);
 void PrintBoard(BOARD *player,int size);
 void InsertBoatM(int np,int npTipos[],BOARD *player,int size,SHIP *ships);
-char ValModo();
+char ValModo(); 
 int ValTab();
 void NPieces(int *nptpieces,int tabu);
 void WaitForPlayer();
